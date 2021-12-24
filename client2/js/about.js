01f5c2211aee49d5156c1a3e1063f2c13dbdeee6
Sam Byers <samwbyers@gmail.com>
2021-12-24 14:40:00 -0500

    split up js files and fix ppl search bug

--- client2/js/about.js
+++ client2/js/about.js
@@ -1,25 +1,14 @@
 // Run when the pages loads
 window.onload = function() {
-    let url = window.location.href
-    // Check if home page with session in browser
+    // Check if about page with session in browser
     if (sessionStorage.getItem('session')) {
         let sessionId = sessionStorage.getItem('session')
         let username = sessionStorage.getItem('username')
         if (username) {
             setLogoutButton(sessionId, 'wbxLoginBtn')
             setUsername('loginIcon', 'loginText', username)
-            searchNameEventListener()
+            forgetMe(sessionId)
         } else { getUser(sessionId) }
-        scheduleMessage(sessionId)
-    }
-    // Check if home with session in url
-    else if (url.includes('session=')) {
-        let session = url.split("?")[1]
-        let sessionId = session.split("=")[1]
-        sessionStorage.setItem('session', sessionId)
-        getUser(sessionId)
-        scheduleMessage(sessionId)
-        searchNameEventListener()
     }
     // Otherwise, make login button
     else {
